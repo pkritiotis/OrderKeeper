dd5053e380ae4f1dfd46beddcb7cee6b81858389
Panayiotis Kritiotis <pkritiotis@gmail.com>
2018-08-26 23:55:03 +0300

    Renamed Customer.APi to CustomerManagement.API Added jwt bearer authentication into the customer api calls

---
+++ src/Services/CustomerManagement.API/Model/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.API.Model
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Task<Customer> AddCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCustomerAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Services/CustomerManagement.API/Startup.cs
+++ src/Services/CustomerManagement.API/Startup.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using CustomerManagement.API.Configuration;
+using CustomerManagement.API.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerManagement.API
@@ -17,19 +22,43 @@ namespace CustomerManagement.API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            JwtConfiguration = new JwtConfiguration();
         }
 
         public IConfiguration Configuration { get; }
+        private JwtConfiguration JwtConfiguration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.GetSection("Jwt").Bind(JwtConfiguration);
+            services.AddSingleton(JwtConfiguration);
             services.AddMvc();
+            services.AddAuthentication(options =>
+             {
+                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                 options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+             })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = JwtConfiguration.Issuer,
+                        ValidAudience = JwtConfiguration.Issuer,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtConfiguration.Key)),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "OrderKeeper Customer Management API", Version = "v1" });
             });
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +68,8 @@ namespace CustomerManagement.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
