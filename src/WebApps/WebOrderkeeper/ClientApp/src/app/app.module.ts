f4fcf484b01726562609f7c576eafaf0cc2e956a
Panayiotis Kritiotis <pkritiotis@gmail.com>
2018-08-28 23:43:53 +0300

    WebOrderKeeper: Added header into the app with sample navigation bar

--- src/WebApps/WebOrderkeeper/ClientApp/src/app/app.module.ts
+++ src/WebApps/WebOrderkeeper/ClientApp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
