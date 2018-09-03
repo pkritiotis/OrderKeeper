3f408dd7c5aab7a41d55c388acdbb660589045ac
Panayiotis Kritiotis <pkritiotis@gmail.com>
2018-09-03 21:23:42 +0300

    Weborderkeeper - clientapp Authentication Cleanup

--- src/WebApps/WebOrderkeeper/ClientApp/src/app/authentication/_services/authentication.service.ts
+++ src/WebApps/WebOrderkeeper/ClientApp/src/app/authentication/_services/authentication.service.ts
@@ -8,24 +8,14 @@ import { ConfigurationService } from '../../shared/services/configuration.servic
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
 
-    constructor(private http: HttpClient, private configurationService: ConfigurationService, private storageService: StorageService, private securityService: SecurityService) { }
+    constructor(private http: HttpClient, private configurationService: ConfigurationService,
+                private storageService: StorageService, private securityService: SecurityService) { }
 
     login(email: string, password: string) {
-        const identityApiUrl = this.configurationService.serverSettings.identityUrl + 'account/login';
-        return this.http.post<any>(identityApiUrl, { email, password })
-            .pipe(map(user => {
-                // login successful if there's a jwt token in the response
-                // if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    this.securityService.IsAuthorized = true;
-                    this.securityService.SetAuthorizationData(user, user);
-                // }
-
-                return user;
-            }));
+        return this.securityService.LogIn(email, password);
     }
 
     logout() {
-        this.storageService.store('currentUser', '');
+        this.securityService.Logoff();
     }
 }
--- src/WebApps/WebOrderkeeper/ClientApp/src/app/shared/services/security.service.ts
+++ src/WebApps/WebOrderkeeper/ClientApp/src/app/shared/services/security.service.ts
@@ -1,5 +1,5 @@
-﻿import { Injectable }                   from '@angular/core';
-import { Http, Response, Headers }      from '@angular/http';
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -11,159 +11,76 @@ import { StorageService } from './storage.service';
 @Injectable()
 export class SecurityService {
 
-    private actionUrl: string;
     private headers: Headers;
-    private storage: StorageService;
-    private authenticationSource = new Subject<boolean>();
-    authenticationChallenge$ = this.authenticationSource.asObservable();
-    private authorityUrl = '';
+    private identityUrl = '';
 
-    constructor(private _http: Http, private _router: Router, private route: ActivatedRoute, private _configurationService: ConfigurationService, private _storageService: StorageService) {
+    public IsAuthorized: boolean;
+    public UserData: any;
+
+    constructor(private _http: Http, private _router: Router,
+        private route: ActivatedRoute, private _configurationService: ConfigurationService,
+        private _storageService: StorageService) {
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.headers.append('Accept', 'application/json');
-        this.storage = _storageService;
 
         this._configurationService.settingsLoaded$.subscribe(x => {
-            this.authorityUrl = this._configurationService.serverSettings.identityUrl;
-            this.storage.store('IdentityUrl', this.authorityUrl);
+            this.identityUrl = this._configurationService.serverSettings.identityUrl;
+            _storageService.store('IdentityUrl', this.identityUrl);
         });
 
-        if (this.storage.retrieve('IsAuthorized') !== '') {
-            this.IsAuthorized = this.storage.retrieve('IsAuthorized');
-            this.authenticationSource.next(true);
-            this.UserData = this.storage.retrieve('userData');
+        if (_storageService.retrieve('IsAuthorized') !== '') {
+            this.IsAuthorized = _storageService.retrieve('IsAuthorized');
+            this.UserData = _storageService.retrieve('userData');
         }
     }
 
-    public IsAuthorized: boolean;
+    LogIn(email, password): any {
+        const identityApiUrl = this._configurationService.serverSettings.identityUrl + 'account/login';
+        return this._http.post(identityApiUrl, { email, password })
+            .map(user => {
+                // login successful if there's a jwt token in the response
+                this.IsAuthorized = true;
+                this.SetAuthorizationData(user, 'jwt');
+                return user;
+            });
+    }
 
     public GetToken(): any {
-        return this.storage.retrieve('authorizationData');
+        return this._storageService.retrieve('authorizationData');
     }
 
     public ResetAuthorizationData() {
-        this.storage.store('authorizationData', '');
-        this.storage.store('authorizationDataIdToken', '');
-
+        this._storageService.store('authorizationData', '');
+        this._storageService.store('authorizationDataIdToken', '');
         this.IsAuthorized = false;
-        this.storage.store('IsAuthorized', false);
+        this._storageService.store('IsAuthorized', false);
     }
 
-    public UserData: any;
     public SetAuthorizationData(token: any, id_token: any) {
-        if (this.storage.retrieve('authorizationData') !== '') {
-            this.storage.store('authorizationData', '');
+        if (this._storageService.retrieve('authorizationData') !== '') {
+            this._storageService.store('authorizationData', '');
         }
 
-        this.storage.store('authorizationData', token);
-        this.storage.store('authorizationDataIdToken', id_token);
+        this._storageService.store('authorizationData', token);
+        this._storageService.store('authorizationDataIdToken', id_token);
         this.IsAuthorized = true;
-        this.storage.store('IsAuthorized', true);
+        this._storageService.store('IsAuthorized', true);
 
         this.getUserData()
             .subscribe(data => {
                 this.UserData = data;
-                this.storage.store('userData', data);
-                // emit observable
-                this.authenticationSource.next(true);
+                this._storageService.store('userData', data);
                 window.location.href = location.origin;
             },
-            error => this.HandleError(error),
-            () => {
-                console.log(this.UserData);
-            });
-    }
-
-    public Authorize() {
-        this.ResetAuthorizationData();
-
-        const authorizationUrl = this.authorityUrl + '/connect/authorize';
-        const client_id = 'js';
-        const redirect_uri = location.origin + '/';
-        const response_type = 'id_token token';
-        const scope = 'openid profile orders basket marketing locations webshoppingagg orders.signalrhub';
-        const nonce = 'N' + Math.random() + '' + Date.now();
-        const state = Date.now() + '' + Math.random();
-
-        this.storage.store('authStateControl', state);
-        this.storage.store('authNonce', nonce);
-
-        const url =
-            authorizationUrl + '?' +
-            'response_type=' + encodeURI(response_type) + '&' +
-            'client_id=' + encodeURI(client_id) + '&' +
-            'redirect_uri=' + encodeURI(redirect_uri) + '&' +
-            'scope=' + encodeURI(scope) + '&' +
-            'nonce=' + encodeURI(nonce) + '&' +
-            'state=' + encodeURI(state);
-
-        window.location.href = url;
-    }
-
-    public AuthorizedCallback() {
-        this.ResetAuthorizationData();
-
-        const hash = window.location.hash.substr(1);
-
-        const result: any = hash.split('&').reduce(function (result: any, item: string) {
-            const parts = item.split('=');
-            result[parts[0]] = parts[1];
-            return result;
-        }, {});
-
-        console.log(result);
-
-        let token = '';
-        let id_token = '';
-        let authResponseIsValid = false;
-
-        if (!result.error) {
-
-            if (result.state !== this.storage.retrieve('authStateControl')) {
-                console.log('AuthorizedCallback incorrect state');
-            } else {
-
-                token = result.access_token;
-                id_token = result.id_token;
-
-                const dataIdToken: any = this.getDataFromToken(id_token);
-                console.log(dataIdToken);
-
-                // validate nonce
-                if (dataIdToken.nonce !== this.storage.retrieve('authNonce')) {
-                    console.log('AuthorizedCallback incorrect nonce');
-                } else {
-                    this.storage.store('authNonce', '');
-                    this.storage.store('authStateControl', '');
-
-                    authResponseIsValid = true;
-                    console.log('AuthorizedCallback state and nonce validated, returning access token');
-                }
-            }
-        }
-
-
-        if (authResponseIsValid) {
-            this.SetAuthorizationData(token, id_token);
-        }
+                error => this.HandleError(error),
+                () => {
+                    console.log(this.UserData);
+                });
     }
 
     public Logoff() {
-        const authorizationUrl = this.authorityUrl + '/connect/endsession';
-        const id_token_hint = this.storage.retrieve('authorizationDataIdToken');
-        const post_logout_redirect_uri = location.origin + '/';
-
-        const url =
-            authorizationUrl + '?' +
-            'id_token_hint=' + encodeURI(id_token_hint) + '&' +
-            'post_logout_redirect_uri=' + encodeURI(post_logout_redirect_uri);
-
         this.ResetAuthorizationData();
-
-        // emit observable
-        this.authenticationSource.next(false);
-        window.location.href = url;
     }
 
     public HandleError(error: any) {
@@ -204,27 +121,14 @@ export class SecurityService {
         return data;
     }
 
-    // private retrieve(key: string): any {
-    //    let item = this.storage.getItem(key);
-
-    //    if (item && item !== 'undefined') {
-    //        return JSON.parse(this.storage.getItem(key));
-    //    }
-
-    //    return;
-    // }
-
-    // private store(key: string, value: any) {
-    //    this.storage.setItem(key, JSON.stringify(value));
-    // }
 
     private getUserData = (): Observable<string[]> => {
         this.setHeaders();
-        if (this.authorityUrl === '') {
-            this.authorityUrl = this.storage.retrieve('IdentityUrl');
+        if (this.identityUrl === '') {
+            this.identityUrl = this._storageService.retrieve('IdentityUrl');
         }
 
-        return this._http.get(this.authorityUrl + '/connect/userinfo', {
+        return this._http.get(this.identityUrl + '/account/userinfo', {
             headers: this.headers,
             body: ''
         }).map(res => res.json());
