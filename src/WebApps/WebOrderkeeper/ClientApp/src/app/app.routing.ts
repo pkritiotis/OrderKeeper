c0125f4e715c9abe653d2569de871895ab9dbac7
Panayiotis Kritiotis <pkritiotis@gmail.com>
2019-01-14 22:49:50 +0200

    WebApp: routing import and path order changes - ocd hit :/

--- src/WebApps/WebOrderkeeper/ClientApp/src/app/app.routing.ts
+++ src/WebApps/WebOrderkeeper/ClientApp/src/app/app.routing.ts
@@ -1,9 +1,9 @@
-import { CustomerDetailComponent } from './customer/customer-detail/customer-detail.component';
-import { CustomerManagementComponent } from './customer/customer-management.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/_guards/auth.guard';
-import { LoginComponent } from './authentication/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { CustomerDetailComponent } from './customer/customer-detail/customer-detail.component';
+import { CustomerManagementComponent } from './customer/customer-management.component';
 import { ProductManagementComponent } from './product/product-management.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { OrderManagementComponent } from './order/order-management.component';
@@ -12,6 +12,7 @@ import { OrderDetailComponent } from './order/order-detail/order-detail.componen
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
     { path: 'customers', component: CustomerManagementComponent, canActivate: [AuthGuard] },
     { path: 'customer/:id/edit', component: CustomerDetailComponent, data: {action: 'edit'}, canActivate: [AuthGuard] },
     { path: 'customer/new', component: CustomerDetailComponent, data: {action: 'create'}, canActivate: [AuthGuard] },
@@ -22,7 +23,6 @@ const appRoutes: Routes = [
     { path: 'order/:id/edit', component: OrderDetailComponent, data: {action: 'edit'}, canActivate: [AuthGuard] },
     { path: 'order/new', component: OrderDetailComponent, data: {action: 'create'}, canActivate: [AuthGuard] },
 
-    { path: 'login', component: LoginComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
